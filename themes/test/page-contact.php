750a04552de3a94c40003171b07972d9044e1127
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-12-07 12:37:06 +0900

    update : header作成（仮）

--- themes/test/page-contact.php
+++ themes/test/page-contact.php
@@ -1,5 +1,9 @@
+<?php get_header(); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
         <?php the_content(); ?>
 <?php endwhile;
 endif; ?>
+
+<?php wp_footer(); ?>
